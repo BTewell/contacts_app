785dd92ec89975696e6a965d645990fa5f000c28
“Brett <bretttewell@gmail.com>
2019-03-04 19:55:35 -0600

    RESTful refactoring - add create route/action/json.

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -9,4 +9,22 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find_by(id: the_id)
     render 'show.json.jbuilder'
   end
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      address: params[:address],
+      phone: params[:phone],
+      email: params[:email]
+      )
+    @contact.save
+    render 'show.json.jbuilder'
+  end
+  def update
+    the_id = params[:id]
+    @contact = Contact.find_by(id: the_id)
+    @contact.first_name = params[:first_name],
+    @contact.last_name
+    render 'show.json.jbuilder'
+  end
 end
