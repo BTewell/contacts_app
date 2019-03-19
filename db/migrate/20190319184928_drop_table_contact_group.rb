8a841f570c8c8b1b4eecc02367c61c12976ee8bc
“Brett <bretttewell@gmail.com>
2019-03-19 14:18:04 -0500

    resolve db migrate issue for cloned apps.

--- db/migrate/20190319184928_drop_table_contact_group.rb
+++ db/migrate/20190319184928_drop_table_contact_group.rb
@@ -1,6 +1,6 @@
 class DropTableContactGroup < ActiveRecord::Migration[5.2]
   def change
-    drop_table :contact_groups
-    drop_table :contacts_groups
+    # drop_table :contact_groups
+    # drop_table :contacts_groups
   end
 end
